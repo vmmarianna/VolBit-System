1e119d3d4d40ca65c58afb1943766cb3fcf72edd
marina <marina9021089@gmail.com>
2020-05-25 20:35:21 +0300

    deploying with PG on heroku

--- volbitengine/settings.py
+++ volbitengine/settings.py
@@ -131,3 +131,7 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
